28566a21ff78652bdffa004b421841b5d5801b66
Taylor Thornton <thornrae@gmail.com>
2020-10-03 15:28:08 -0700

    lab3 additions

--- js/app.js
+++ js/app.js
@@ -1,54 +1,101 @@
 'use strict';
 
+
 var userName = prompt('Hi! What is your name?');
 console.log(userName);
 
-// Hometown Question
-var homeTown = prompt('Is Taylor from Seattle?').toLowerCase();
-console.log(homeTown);
+var correctAnswer=0;
 
-if (homeTown === 'yes' || homeTown === 'y') {
-  // console.log('Correct! Taylor is from Seattle');
-  alert('Correct, ' + userName + '! Taylor is from Seattle');
+// // Hometown Question
+// var homeTown = prompt('Is Taylor from Seattle?').toLowerCase();
+// console.log(homeTown);
+
+// if (homeTown === 'yes' || homeTown === 'y') {
+//   // console.log('Correct! Taylor is from Seattle');
+//   alert('Correct, ' + userName + '! Taylor is from Seattle');
+//   correctAnswer++;
+// } else {
+//   alert('You got that wrong, ' + userName + '.');
+// }
+
+
+// // Profession Question
+// var profession = prompt('Is Taylor a software developer?').toLowerCase();
+// console.log(profession);
+
+// if (profession === 'yes' || profession === 'y'){
+//   // console.log('Correct! Taylor is a software developer');
+//   alert('Correct, ' + userName + '! Taylor is a software developer');
+//   correctAnswer++;
+// }else {
+//   alert('You got that wrong, ' + userName + '.');
+// }
+
+// // Operating System Question
+// var operatingSystem = prompt('Is Taylor a MacOS user?').toLowerCase();
+// console.log(operatingSystem);
+
+// if (operatingSystem === 'yes' || operatingSystem === 'y'){
+//   // console.log('Correct! Taylor is a MacOS user');
+//   alert('Correct, ' + userName + '! Taylor is a MacOS user');
+//   correctAnswer++;
+// } else {
+//   alert('You got that wrong, ' + userName + '.');
+// }
+
+// // Houseplants
+// var housePlants = prompt('Does Taylor have house plants?').toLowerCase();
+// console.log(housePlants);
+
+// if(housePlants === 'yes' || housePlants === 'y'){
+//   // console.log('Correct! Taylor has house plants');
+//   alert('Correct, ' + userName + '! Taylor has house plants');
+//   correctAnswer++;
+// } else {
+//   alert('You got that wrong, ' + userName + '.');
+// }
+
+// // Travel
+// var travel = prompt('Has Taylor traveled out of the country?').toLowerCase();
+// console.log(travel);
+
+// if(travel === 'yes' || travel === 'y'){
+//   // console.log('Correct! Taylor has traveled out of the country');
+//   alert('Correct, ' + userName + '! Taylor has traveled out of the country');
+//   correctAnswer++;
+// } else {
+//   alert('You got that wrong, ' + userName + '.');
+// }
+
+// How many countries?
+
+var numberOfCountries = prompt('How many countries has Taylor travelled to? Please answer in numberic value.');
+console.log(numberOfCountries);
+
+var countryGuesses = 0;
+
+while (countryGuesses <= 4) {
+  if (numberOfCountries > 7){
+    prompt('Too high, ' + userName + ', guess again!');
+    countryGuesses++;
+  } else if (numberOfCountries < 7){
+    prompt('Too low, ' + userName + ' guess again!');
+    countryGuesses++;
+  } else if (numberOfCountries === 7){
+    alert('Correct! ' + userName + '.' );
+    correctAnswer++;
+    countryGuesses++;
+    break;
+  // } else {
+  //   break;
+  // }
 }
 
-// Profession Question
-var profession = prompt('Is Taylor a software developer?').toLowerCase();
-console.log(profession);
 
-if (profession === 'yes' || profession === 'y'){
-  // console.log('Correct! Taylor is a software developer');
-  alert('Correct, ' + userName + '! Taylor is a software developer');
-}
 
-// Operating System Question
-var operatingSystem = prompt('Is Taylor a MacOS user?').toLowerCase();
-console.log(operatingSystem);
 
-if (operatingSystem === 'yes' || operatingSystem === 'y'){
-  // console.log('Correct! Taylor is a MacOS user');
-  alert('Correct, ' + userName + '! Taylor is a MacOS user');
-}
 
-// Houseplants
-var housePlants = prompt('Does Taylor have house plants?').toLowerCase();
-console.log(housePlants);
-
-if(housePlants === 'yes' || housePlants === 'y'){
-  // console.log('Correct! Taylor has house plants');
-  alert('Correct, ' + userName + '! Taylor has house plants');
-}
-
-// Travel
-var travel = prompt('Has Taylor traveled out of the country?').toLowerCase();
-console.log(travel);
-
-if(travel === 'yes' || travel === 'y'){
-  // console.log('Correct! Taylor has traveled out of the country');
-  alert('Correct, ' + userName + '! Taylor has traveled out of the country');
-}
 
 //Final Message to User
-var finalMessage = alert('Thanks for your guesses, ' + userName + ', I hope you were able to learn a little more about Taylor.');
+var finalMessage = alert('Thanks for your guesses, ' + userName + ', I hope you were able to learn a little more about Taylor.  You got ' + correctAnswer + ' answers right out of 7.');
 console.log(finalMessage);
-
